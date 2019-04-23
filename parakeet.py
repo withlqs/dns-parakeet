7ff9e29dd08eda2d562ce722349941cf4e94b4db
Qiushi Li <withlqs@gmail.com>
2019-04-23 15:22:20 +0800

    randomize transaction id

--- parakeet.py
+++ parakeet.py
@@ -2,33 +2,47 @@
 
 import argparse
 import json
+import os
 import socket
 import socketserver
 
 dns_server_list = []
 
 
-class UDPMessageHandler(socketserver.BaseRequestHandler):
+class DNSRequestHandler(socketserver.BaseRequestHandler):
+
+    @staticmethod
+    def rand_request(request):
+        return os.urandom(2) + request[2:]
+
+    @staticmethod
+    def restore_request(request, trans_id):
+        return trans_id + request[2:]
 
     def handle(self):
+        trans_id = self.request[0][0:2]
+        # print(type(self.request[0]))
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(0.05)
+        sock.settimeout(0.03)
 
         for dns_server in dns_server_list:
-            sock.sendto(self.request[0], (dns_server, 53))
+            sock.sendto(DNSRequestHandler.rand_request(self.request[0]), (dns_server, 53))
         for dns_server in dns_server_list:
-            sock.sendto(self.request[0], (dns_server, 53))
+            sock.sendto(DNSRequestHandler.rand_request(self.request[0]), (dns_server, 53))
 
         while True:
             try:
-                result = sock.recv(512)
+                result, address = sock.recvfrom(512)
+                print(address)
+                # print(result)
             except socket.timeout:
+                print('timeout')
                 for dns_server in dns_server_list:
-                    sock.sendto(self.request[0], (dns_server, 53))
+                    sock.sendto(DNSRequestHandler.rand_request(self.request[0]), (dns_server, 53))
                 continue
             break
 
-        self.request[1].sendto(result, self.client_address)
+        self.request[1].sendto(DNSRequestHandler.restore_request(result, trans_id), self.client_address)
         sock.close()
 
 
@@ -47,7 +61,7 @@ def main():
     for server in read_server_list:
         dns_server_list.append(server)
 
-    server = socketserver.ThreadingUDPServer(('127.0.0.1', 53), UDPMessageHandler)
+    server = socketserver.ThreadingUDPServer(('127.0.0.1', 53), DNSRequestHandler)
     server.serve_forever()
 
 
