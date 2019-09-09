b3340bf90161c5e29c8e83380e2bd05426b1a61b
Qiushi Li <withlqs@gmail.com>
2019-09-09 19:58:08 +0800

    TODO: TCP Request

--- parakeet.py
+++ parakeet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import datetime
 import json
 import os
 import socket
@@ -8,9 +9,31 @@ import socketserver
 
 dns_server_list = []
 debug = False
-latency = 0.029
-max_message_length = 1024
+latency = 0.013
+max_message_length = 512
 max_waiting_time = 2
+first_send = 2
+
+
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def colored(content, color=None):
+    content = str(content)
+    if color is not None:
+        buf = color + content + Colors.ENDC
+    else:
+        buf = content
+
+    return buf
 
 
 class DNSRequestHandler(socketserver.BaseRequestHandler):
@@ -26,34 +49,58 @@ class DNSRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         trans_id = self.request[0][0:2]
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(latency)
 
-        #for dns_server in dns_server_list:
-        #    sock.sendto(self.request[0], (dns_server, 53))
-        for dns_server in dns_server_list:
-            sock.sendto(DNSRequestHandler.rand_request(self.request[0]), (dns_server, 53))
+        if debug:
+            start = datetime.datetime.now()
+        for i in range(first_send):
+            for dns_server in dns_server_list:
+                sock.sendto(DNSRequestHandler.rand_request(self.request[0]), (dns_server, 53))
+
+        sock.settimeout(latency)
+        buf = []
+
+        def push_buf(content, buf_list=buf):
+            buf_list.append(str(content) + '\n')
+
+        def merge_buf(buf_list=buf):
+            return ''.join(buf_list)
+
+        if debug:
+            domain = ''
+            now = 12
+            length = self.request[0][now]
+            while length != 0:
+                domain += bytes(self.request[0][now + 1:now + 1 + length]).decode('utf-8') + '.'
+                now = now + 1 + length
+                length = self.request[0][now]
+
+            push_buf(
+                colored('domain:\t{}'.format(domain), Colors.WARNING)
+            )
 
         timeout = latency
         while True:
             try:
                 result, address = sock.recvfrom(max_message_length)
                 if debug:
-                    print('result\t{}'.format(result[0:2]))
-                    print('trans_id\t{}'.format(trans_id))
-                    print(address)
+                    push_buf('length:\t{} bytes'.format(len(result)))
+                    push_buf('server:\t{0}:{1}'.format(address[0], address[1]))
                 self.request[1].sendto(DNSRequestHandler.restore_request(result, trans_id), self.client_address)
                 break
             except Exception as ex:
-                if debug:
-                    print(type(ex))
                 for dns_server in dns_server_list:
                     sock.sendto(DNSRequestHandler.rand_request(self.request[0]), (dns_server, 53))
                 timeout += latency
                 if timeout > max_waiting_time:
                     if debug:
-                        print('timeout and waited %.3f' % timeout)
+                        push_buf(colored('Warning:\ttimeout', Colors.FAIL))
                     break
 
+        if debug:
+            end = datetime.datetime.now()
+            duration = int((end - start).total_seconds() * 1000)
+            push_buf('time:\t' + colored('{}ms'.format(duration), Colors.UNDERLINE))
+            print(merge_buf())
         sock.close()
 
 
